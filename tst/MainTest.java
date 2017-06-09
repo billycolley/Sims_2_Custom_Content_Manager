d78ed2c200084be55cdd22a6a3204ad4ba3c5a9a
billycolley <billy.d.colley@gmail.com>
2017-06-08 21:54:02 -0700

    Initial Commit With Main and MainTest.

---
+++ tst/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by krovl on 6/8/2017.
+ */
+public class MainTest {
+    @Test
+    public void simpleTest() {
+        System.out.println("This is lame");
+        Main.main(null);
+    }
+}
